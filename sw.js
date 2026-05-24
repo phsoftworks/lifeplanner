b5eaf757e926ea81ae221277249a010e95246256
phsoftworks <pherreroga@gmail.com>
2026-05-24 19:04:39 +0200

    update

--- sw.js
+++ sw.js
@@ -1,25 +1,25 @@
-const CACHE_NAME = "lifeplanner-v1";
+const CACHE_NAME = "lifeplanner-v2";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./style.css",
   "./script.js",
-  "./manifest.json"
+  "./manifest.json",
+  "./icon-192-v2.png",
+  "./icon-512-v2.png"
 ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
     })
   );
 });
