f4488215defbff36db8c8d8f57eb7fdf5431896a
phsoftworks <pherreroga@gmail.com>
2026-05-24 12:13:54 +0200

    update

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ function addTask() {
 
   li.addEventListener("click", function() {
     li.classList.toggle("completed");
+    saveTasks();
   });
 
   const deleteBtn = li.querySelector(".delete-btn");
@@ -20,9 +21,53 @@ function addTask() {
   deleteBtn.addEventListener("click", function(event) {
     event.stopPropagation();
     li.remove();
+    saveTasks();
   });
 
   document.getElementById("taskList").appendChild(li);
-
+saveTasks();
   input.value = "";
 }
+function saveTasks() {
+  const tasks = [];
+
+  document.querySelectorAll("#taskList li").forEach(li => {
+    tasks.push({
+      text: li.childNodes[0].textContent.trim(),
+      completed: li.classList.contains("completed")
+    });
+  });
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+window.onload = function() {
+  const savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+  savedTasks.forEach(task => {
+    const li = document.createElement("li");
+
+    li.innerHTML = `
+      ${task.text}
+      <button class="delete-btn">❌</button>
+    `;
+
+    if (task.completed) {
+      li.classList.add("completed");
+    }
+
+    li.addEventListener("click", function() {
+      li.classList.toggle("completed");
+      saveTasks();
+    });
+
+    const deleteBtn = li.querySelector(".delete-btn");
+
+    deleteBtn.addEventListener("click", function(event) {
+      event.stopPropagation();
+      li.remove();
+      saveTasks();
+    });
+
+    document.getElementById("taskList").appendChild(li);
+  });
+};
